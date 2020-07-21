ab105bb9ec07561fe23d977223eda6f184a840ed
ninjahawk <40222252+ninjahawk@users.noreply.github.com>
2020-07-21 03:08:07 -0400

    Add files via upload

---
+++ alertmaker.py
@@ -0,0 +1,15 @@
+Python 3.8.2 (default, Apr 27 2020, 15:53:34) 
+[GCC 9.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from tkinter import *
+>>> from tkinter import messagebox
+>>> from tkinter import Button
+>>> from tkinter import Tk, TOP
+>>> def callback():
+	root = Tk()
+	root.withdraw()
+	messagebox.showinfo('Doop', 'Hello There')
+
+	
+>>> boot = Tk()
+>>> b = Button(boot, text='Press me!', command = callback).pack(side = TOP)
---
+++ buttonmaker.py
@@ -0,0 +1,13 @@
+Python 3.8.2 (default, Apr 27 2020, 15:53:34) 
+[GCC 9.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from tkinter import *
+>>> from tkinter import Button
+>>> from tkinter import Tk, TOP
+>>> from tkinter import messagebox
+>>> def callback():
+	root = Tk()
+
+	
+>>> doop = Tk()
+>>> b = Button(doop, text='Hello There', command=callback).pack(side=TOP)
