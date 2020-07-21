e3a1e8a2d1be14d1be9ed6b6fcd1692b159f75e7
ninjahawk <40222252+ninjahawk@users.noreply.github.com>
2020-07-21 03:12:50 -0400

    Update alertmaker.py

--- alertmaker.py
+++ alertmaker.py
@@ -1,6 +1,10 @@
 Python 3.8.2 (default, Apr 27 2020, 15:53:34) 
 [GCC 9.3.0] on linux
 Type "help", "copyright", "credits" or "license()" for more information.
+
+#This program will make an alert appear 
+#on screen when the button is pressed.
+
 >>> from tkinter import *
 >>> from tkinter import messagebox
 >>> from tkinter import Button
