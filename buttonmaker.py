74c6c4c27199f94632084ddf9f04c036956fe410
ninjahawk <40222252+ninjahawk@users.noreply.github.com>
2020-07-21 03:15:39 -0400

    Update buttonmaker.py

--- buttonmaker.py
+++ buttonmaker.py
@@ -1,6 +1,11 @@
 Python 3.8.2 (default, Apr 27 2020, 15:53:34) 
 [GCC 9.3.0] on linux
 Type "help", "copyright", "credits" or "license()" for more information.
+
+#This program will create an infinite amount of messageboxes 
+#as the button is repeatedly pressed. Not much use practically,
+#except for maybe getting on your computer teacher's nerves. 
+
 >>> from tkinter import *
 >>> from tkinter import Button
 >>> from tkinter import Tk, TOP
